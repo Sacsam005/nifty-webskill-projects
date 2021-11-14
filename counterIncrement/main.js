1b5b32999e8fd3eda61e09d180186c0ca828d68f
Sachin Samal <88958992+Sacsam005@users.noreply.github.com>
2021-11-13 19:36:23 -0600

    Add files via upload

---
+++ counterIncrement/main.js
@@ -0,0 +1,23 @@
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach((counter) => {
+
+    counter.innerHTML = 0;
+
+    const updateCounter = () => {
+        const targetCount = +counter.getAttribute('data-target');
+
+    const startCount = Number(counter.innerHTML);
+
+    const incr = targetCount/100;
+
+    if(startCount < targetCount) {
+        counter.innerHTML = `${Math.round(startCount + incr)}`;
+        setTimeout(updateCounter, 100)
+    } else {
+        counter.innerHTML = targetCount;
+    }
+    }
+    
+    updateCounter();
+})
